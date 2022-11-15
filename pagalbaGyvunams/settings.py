fe9ea06d3179d76ac88405e2215155dbafc8f3d2
arvydasg <azegaspa@gmail.com>
2022-11-15 21:40:43 +0200

    getting csrf error, adding trusted origins

--- pagalbaGyvunams/settings.py
+++ pagalbaGyvunams/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = "django-insecure-quuu*664%azht!3&!npfy*fz#&yhn$b#5vpd_nizw31r1peeye
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://pagalbagyvunams-production.up.railway.app/']
 
 
 # Application definition
